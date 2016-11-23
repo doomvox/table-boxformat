6466146ecac14cd328e7c06380566e7c4b6040b8
Joseph Brenner <doomvox@gmail.com>
2016-11-22 20:09:48 -0800

    Got experimental read_exp working with fixed-width approach

--- lib/Data/BoxFormat.pm
+++ lib/Data/BoxFormat.pm
@@ -13,7 +13,11 @@ Version 0.01
 =cut
 
 our $VERSION = '0.01';
-my $DEBUG = 1;              # TODO revise before shipping
+my  $DEBUG   = 1;          # TODO revise before shipping
+use 5.008;
+use Carp;
+use Data::Dumper;
+use utf8::all;
 
 =head1 SYNOPSIS
 
@@ -29,11 +33,8 @@ my $DEBUG = 1;              # TODO revise before shipping
 
 
    # input from dbox file, output directly to a tsv file
-   my $dbx = Data::BoxFormat->new( input_file  => '/tmp/select_result.dbox',
-                               output_file => '/tmp/select_result.tsv',
-                             );
-   $dbx->read2tsv();
-
+   my $dbx = Data::BoxFormat->new( input_file  => '/tmp/select_result.dbox' );
+   $dbx->read2tsv( '/tmp/select_result.tsv' );
 
    # TODO test this
    # Use just a unicode character as sepatator via a custom
@@ -46,8 +47,7 @@ my $DEBUG = 1;              # TODO revise before shipping
                                  \N{BOX DRAWINGS LIGHT VERTICAL}
                                     {1,1}
                                 \s+
-                            }xms }
-                       );
+                              }xms } );
    my $data = $dbx->read(); # array of arrays, header in first row
 
 
@@ -140,19 +140,73 @@ U+253C ‹┼› \N{BOX DRAWINGS LIGHT VERTICAL AND HORIZONTAL}
        Grapheme_Base Graph GrBase Other_Symbol Pat_Syn Pattern_Syntax PatSyn
        Print Symbol Unicode X_POSIX_Graph X_POSIX_Print
 
+=head1 REGEXP PROPERTIES
 
+Defining some custom regexp character properties.
+(A neat trick, but for our purposes this gains us nothing:
+these will go away in later versions.
 
+=over
+
+=cut
+
+=item IsHor
+
+Matches characters found in a "horizontal rule" row.
+
+=cut
+
+# defining character properties for regexp defaults
+sub IsHor {
+  my @codepoints =
+    ('002D',  # -  \N{HYPHEN-MINUS}
+     '002B',  # +  \N{PLUS SIGN}
+     '2500',  # ─  \N{BOX DRAWINGS LIGHT HORIZONTAL}
+     '253C',  # ┼  \N{BOX DRAWINGS LIGHT VERTICAL AND HORIZONTAL}
+     );
+  my $list = join "\n", @codepoints;
+  return $list;
+}
+
+=item IsCross
+
+Matches the "cross" characters used at line intersections.
+
+=cut
+
+sub IsCross {
+  my @codepoints =
+    (
+     '002B',  # +  \N{PLUS SIGN}
+     '253C',  # ┼  \N{BOX DRAWINGS LIGHT VERTICAL AND HORIZONTAL}
+     );
+  my $list = join "\n", @codepoints;
+  return $list;
+}
+
+=item IsDelim
+
+Matches the delimeter/separator characters used on column boundaries.
+
+=cut
+
+sub IsDelim {
+  my @codepoints =
+    (
+     '007C',  # |  \N{VERTICAL LINE}
+     '2502',  #  │  \N{BOX DRAWINGS LIGHT VERTICAL}
+    );
+  my $list = join "\n", @codepoints;
+  return $list;
+}
+
+=back
 
 =head1 METHODS
 
 =over
 
 =cut
-
-use 5.008;
-use Carp;
-use Data::Dumper;
-use utf8::all;
 
 =item new
 
@@ -175,10 +229,6 @@ Must work as a perl ":encoding(...)" layer.
 =item input_data
 
 SQL SELECT output in the fixed-width-plus-delimiter form discussed above.
-
-=item output_file
-
-Like L<input_file>, only used by L<read2tsv> (at present)
 
 =item output_encoding
 
@@ -225,9 +275,9 @@ has input_file  => ( is => 'rw', isa => Str, default => "" );
 # input encoding defaults to utf-8 (might need to change, e.g. ISO-8859-1)
 has input_encoding => ( is => 'rw', isa => Str, default => 'UTF-8' );
 
-# only used by read2tsv (at present)
-has output_file     => ( is => 'rw', isa => Str, default => "" );
-### TODO lazy default, generate from the input_file, change extension to tsv
+# # only used by read2tsv (at present)
+# has output_file     => ( is => 'rw', isa => Str, default => "" );
+# ### TODO lazy default, generate from the input_file, change extension to tsv
 
 has output_encoding => ( is => 'rw', isa => Str, default => 'UTF-8' );
 
@@ -242,7 +292,7 @@ has input_data  => ( is => 'rw', isa => Str,
                        "Needs either an input data file name ('input_file'), " .
                        "or a multiline string ('input_data')  ";
                    }
-                    my $in_enc = "<:encoding($input_encoding)";
+                   my $in_enc = "<:encoding($input_encoding)";
                    open my $fh, $in_enc, $input_file or croak "$!";
                    local $/; # localized slurp mode
                    my $data = <$fh>;
@@ -252,38 +302,6 @@ has input_data  => ( is => 'rw', isa => Str,
 # TODO would be better if it avoided slurping, and worked line-at-a-time
 
 has header => ( is => 'rw', isa => ArrayRef, default => sub{ [] } );
-
-# defining character properties for regexp defaults
-sub IsHor {
-  my @codepoints =
-    ('002D',  # -  \N{HYPHEN-MINUS}
-     '002B',  # +  \N{PLUS SIGN}
-     '2500',  # ─  \N{BOX DRAWINGS LIGHT HORIZONTAL}
-     '253C',  # ┼  \N{BOX DRAWINGS LIGHT VERTICAL AND HORIZONTAL}
-     );
-  my $list = join "\n", @codepoints;
-  return $list;
-}
-
-sub IsCross {
-  my @codepoints =
-    (
-     '002B',  # +  \N{PLUS SIGN}
-     '253C',  # ┼  \N{BOX DRAWINGS LIGHT VERTICAL AND HORIZONTAL}
-     );
-  my $list = join "\n", @codepoints;
-  return $list;
-}
-
-sub IsDelim {
-  my @codepoints =
-    (
-     '007C',  # |  \N{VERTICAL LINE}
-     '2502',  #  │  \N{BOX DRAWINGS LIGHT VERTICAL}
-    );
-  my $list = join "\n", @codepoints;
-  return $list;
-}
 
 # TODO
 # but don't *really* want to allow mix-and-match delims, it's always one or the other throughout.
@@ -298,7 +316,7 @@ has delimiter_re  => ( is => 'rw', isa => RegexpRef,
 
 
 has horizontal_re => ( is => 'rw', isa => RegexpRef,
-                       default => sub{ qr{ \p{IsHor}+ }x } );
+                       default => sub{ qr{ ^ \p{IsHor}+  $ }x } );
 
 # TODO
 #   horizontal rules have crosses that mark-the-spots of col borders
@@ -335,9 +353,7 @@ in the object's L<header>.
 
 =cut
 
-
-### TODO add features to look for bad data formats
-### TODO even better: be more forgiving of values with embedded delim chars.
+### TODO be more forgiving of values with embedded delim chars.
 sub read {
   my $self = shift;
 
@@ -350,8 +366,13 @@ sub read {
    my $left_edge_re  = $self->left_edge_re;
    my $right_edge_re = $self->right_edge_re;
 
-   $input_data =~ s{ ^ \s* \+       }{}xmsg;
-   $input_data =~ s{ \+ \s* $       }{}xmsg;
+# BUG (?): will never match, because of the \+ stuff
+#    $input_data =~ s{ ^ \s* \+       }{}xmsg;
+#    $input_data =~ s{ \+ \s* $       }{}xmsg;
+   $input_data =~ s{ ^ \s*        }{}xmsg;
+   $input_data =~ s{   \s* $       }{}xmsg;
+
+
    $input_data =~ s{ $left_edge_re  }{}xmsg;
    $input_data =~ s{ $right_edge_re }{}xmsg;
 
@@ -391,21 +412,129 @@ sub read {
 }
 
 
+# Experimental version: try to develop "parse" that
+# uses all information, including column locations.
+# Also, could try to do it single-pass (or closer to it).
+# Compromise: line-at-a-time but allow backtracking on a line.
+sub read_exp {
+  my $self = shift;
+
+  my $input_data    = $self->input_data;
+  my $horizontal_re = $self->horizontal_re;
+  my $delimiter_re  = $self->delimiter_re;
+
+  my $cross_re      = $self->cross_re;
+
+   my $left_edge_re  = $self->left_edge_re;
+   my $right_edge_re = $self->right_edge_re;
+
+  # Here we just look for lines with delimiters on them (skipping
+  # anything else) and then split the lines on the delimiters,
+  # trimming whitespace from the boundaries of all values
+
+  my @lines = split /\n/, $input_data;
+
+  my ( @data, @dividers );
+
+  # we use the crosses in the horizontal rule line to identify column boundaries
+  my @pos;
+ HEADSCAN:
+  foreach my $line ( @lines ) { # TODO: $line is aliased back into @lines...
+#     # strip leading and trailing spaces (as in the next loop-- see below)
+#     $line =~ s{ ^ \s*        }{}xms;
+#     $line =~ s{   \s* $      }{}xms;
+
+    if( $line =~ m{ $horizontal_re }x ) {
+
+      # on mysql-style, want to strip leading and trailing crosses
+      $line =~ s{ ^ $cross_re  }{}xms;
+      $line =~ s{ $cross_re $  }{}xms;
+
+      # 'index' be dumb:
+      #  o  it can't use a regexp: it's limited to character matches.
+      #  o  returns -1 on failure (what's wrong with undef?)
+
+      my @cross_candy =
+        ( "\N{PLUS SIGN}",  # ye olde ascii '+'
+          "\N{BOX DRAWINGS LIGHT VERTICAL AND HORIZONTAL}" ); # newfangled '┼'
+
+      my $cross;
+      foreach my $candy ( @cross_candy ) {
+        my $pat = '\\' . $candy;  # need to backwhack the + char
+        if ( $line =~ m{ $pat }x ) {
+          $cross = $candy;
+        }
+      }
+
+      my $pos = 0;
+      while( ( $pos = index( $line, $cross, $pos ) ) > -1 ){
+        push @pos, $pos;
+        $pos++;
+      }
+      last HEADSCAN;
+    }
+  }
+
+ LINE:
+  foreach my $line ( @lines ) {
+#     # strip leading and trailing whitespace
+#     $line =~ s{ ^ \s*        }{}xms;
+#     $line =~ s{   \s* $      }{}xms;
+
+    # trim the left and right borders (if any)
+    # (converts mysql-style lines into psql-style lines)
+    $line =~ s{ $left_edge_re  }{}xms;
+    $line =~ s{ $right_edge_re }{}xms;
+
+    next LINE if( $line =~ m{ $horizontal_re }x );
+
+    my @vals;
+    my $beg = 0;
+    foreach my $pos ( @pos ) {
+      my $val =
+        substr( $line, $beg, ($pos-$beg-1) );
+      # strip leading and trailing spaces
+      $val =~ s/^\s+//;
+      $val =~ s/\s+$//;
+      push @vals, $val;
+      $beg = $pos + 1;
+    }
+
+    # Now do the last one (through the end of the line)
+    my $val =
+      substr( $line, $beg );
+    # strip leading and trailing spaces
+    $val =~ s/^\s+//;
+    $val =~ s/\s+$//;
+    push @vals, $val;
+
+    # array_of_array format (header treated like any other vals)
+    push @data, \@vals;
+
+    my @header;
+    @header = @{ $data[0] } if @data;
+    $self->header( \@header );
+  }
+  return \@data;
+}
+
+
 
 =item read2tsv
 
 A convenience method that runs L<read> and writes the data to a
-tsv file specified by the field L<output_file>.
+tsv file specified by the given argument.
 
 =cut
 
+# TODO if no output_file is supplied as argument, could fall back
+#      to using the input_file with extension changed to "tsv".
 sub read2tsv {
   my $self = shift;
 
-  my $output_file = $self->output_file;
+  my $output_file = shift;
   unless( $output_file ) {
     croak("read2tsv requires the output_file.");
-    ## TODO could use the input_file (if defined) changing the extension to tsv.
   }
   my $output_encoding = $self->output_encoding;
 
@@ -431,44 +560,85 @@ sub read2tsv {
 Joseph Brenner, E<lt>doom@kzsu.stanford.eduE<gt>,
 05 Jun 2016
 
-=head1 BUGS
+=head1 LIMITATIONS
 
-Please report any bugs or feature requests to
-C<bug-data-boxes at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Data-BoxFormat>.
+=head2 memory limited
 
-I will be notified, and then you'll automatically be notified of
-progress on your bug as I make changes.
+As implemented, this presumes the entire data set can be held in memory.
+Future versions may be more stream-oriented: there's no technical reason
+this couldn't be done.
 
-=head1 SUPPORT
+=head2 delimiters in data could confuse things
 
-You can find documentation for this module with the perldoc command.
+If delimiter characters (e.g. a veritcal bar) are present in a
+string in the data, that could confuse the simple version of
+parsing that this does (particularly if the embedded delimiter
+char was bracketed by whitespace).
 
-    perldoc Emacs::Run
+=head2 what you get is what you get
 
-You can also look for information at:
+This only covers three input formats, and isn't easily extensible
+to handle any others.  A plugin system (ala DBI/DBD) would be overkill.
 
-=over 4
+=head2 sqlite
 
-=item * RT: CPAN's request tracker
+This code does not support the output from sqlite.
+Let me explain why (so I'll stop thinking about adding it):
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Emacs-Run>
+The output format in sqlite3 is actually fairly flexible,
+it has a number of different customization settings.
+The obvious thing to want to support though, is the default format,
+which looks like this:
 
-=item * AnnoCPAN: Annotated CPAN documentation
+  SELECT * from expensoids;
+  |2010-09-01|factory|146035.0
+  |2010-10-01|factory|208816.0
+  |2010-11-01|factory|218866.0
+  |2010-12-01|factory|191239.0
+  |2011-01-01|factory|191239.0
+  |2010-09-01|marketing|467087.0
+  |2010-10-01|marketing|409430.0
 
-L<http://annocpan.org/dist/Emacs-Run>
+This is delimited by an ascii vertical bar, but without any
+bracketing spaces, and without any attempt at using fixed width
+columns.  Note that the left edge has a vertical bar, but the
+right edge does not.  Perhaps the most annoying feature from my
+point of view though, is that there is no header: it's easy
+enough to write code that assumes that there will never be a
+header, and to just leave the header row blank, but if I were
+actually working with sqlite a lot I would turn on the header
+display:
 
-=item * CPAN Ratings
+  .header on
 
-L<http://cpanratings.perl.org/d/Emacs-Run>
+Trying to distguish whether the first row is a header is not
+something I'd want try to do from textual analysis, but adding
+some other way of doing it is not something I want to bother with
+just to support sqlite.  So the question would be: should I assume
+the default format, or the format I'm more likely to use?
 
-=item * Search CPAN
+But if I *were* working with sqlite a lot I wouldn't stop with
+turning on the header, I'd probably use the mode column setting
+also:
 
-L<http://search.cpan.org/dist/Emacs-Run/>
+  .header on
+  .mode column
 
-=back
+That yields output that looks like this:
 
-=cut
+  id          date        type        amount
+  ----------  ----------  ----------  ----------
+  1           2010-09-01  factory     146035.0
+  2           2010-10-01  factory     208816.0
+  3           2010-11-01  factory     218866.0
+
+And that's sufficiently different from everything else in the
+world, and far enough away from the sqlite default most people
+are likely to use, that I'm going to give up on this entirely.
+
+One of the problems here for me is that I simply don't work with
+sqlite very much (why would you, when you could be using
+postgresql?), so I'm resolving to not think about this again.
 
 =head1 ACKNOWLEDGEMENTS
 
